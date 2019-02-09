3d0d548ee434e02fb76a6c7adf37ee6f1332653e
fancn <fanyj@yikeer.com>
2019-02-09 16:07:22 +0800

    products page added

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 // home
-router.get('/', function(req, res, next) {
-	res.render('v1_home')
-})
+router
+	.get('/', function(req, res, next) {
+		res.render('v1_home')
+	})
+	.get('/products', function(req, res, next) {
+		res.render('v1_products')
+	})
 
 module.exports = router
